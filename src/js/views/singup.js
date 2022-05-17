ded9dab8aa8f97920542eb97f667407372220f85
lucciii33 <angelomaiele@gmail.com>
2022-05-17 14:28:14 +0000

    singup almots done

--- src/js/views/singup.js
+++ src/js/views/singup.js
@@ -2,13 +2,117 @@ import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
+import { MDBInput } from 'mdb-react-ui-kit';
+import logo from "../../img/logoname.png";
+import vector from "../../img/modal.png";
+
+import {
+	MDBBtn,
+	MDBModal,
+	MDBModalDialog,
+	MDBModalContent,
+	MDBModalHeader,
+	MDBModalTitle,
+	MDBModalBody,
+	MDBModalFooter,
+} from 'mdb-react-ui-kit';
 
 export const SingUp = props => {
 	const { store, actions } = useContext(Context);
 	const params = useParams();
+	const [centredModal, setCentredModal] = useState(false);
+	const toggleShow = () => setCentredModal(!centredModal);
 	return (
 		<div className="signupGradient">
-			
+			<div className="bgwhite m-auto">
+
+				{/* /////// first part logo and icon*/}
+				<div className="d-flex justify-content-between p-2">
+					<img src={logo} className="logosingup"></img>
+					<div><i class="fas fa-times"></i></div>
+				</div>
+
+				{/* /////// second part titles*/}
+
+				<div className="text-center">
+					<h2  className="titlesingup">Welcome to Elana</h2>
+					<p className="titlesingup">Already have an account? Click <a>here</a> to login</p>
+				</div>
+
+				{/* /////// third part inputs*/}
+
+				<div className="d-flex justify-content-evenly m-2">
+					<div>
+						<label className="labelsingup text-muted">First Name</label>
+						<MDBInput id='form1' type='text' />
+					</div>
+
+
+					<div>
+						<label className="labelsingup text-muted">Last Name</label>
+						<MDBInput id='form1' type='text' />
+					</div>
+				</div>
+				<div className="d-flex justify-content-evenly m-2">
+					<div>
+						<label className="labelsingup text-muted">Practice Name</label>
+						<MDBInput id='form1' type='text' />
+					</div>
+
+
+					<div>
+						<label className="labelsingup text-muted">Practice Number</label>
+						<MDBInput id='form1' type='text' />
+					</div>
+				</div>
+				<div className="d-flex justify-content-evenly m-2">
+					<div>
+						<label className="labelsingup text-muted">Email (This will be your username)</label>
+						<MDBInput id='form1' type='text' />
+					</div>
+
+					<div>
+						<label className="labelsingup text-muted">Password</label>
+						<MDBInput id='form1' type='text' />
+					</div>
+
+				</div>
+				<div className="m-2 ">
+					<label className="labelsingup text-muted">Licensing credentials</label>
+					<MDBInput id='form1' type='text'/>
+				</div>
+				<div className="m-2">
+					<label className="labelsingup text-muted">Area of Specialty</label>
+					<MDBInput id='form1' type='text' className="inputsingup"/>
+				</div>
+
+				{/* /////// fourth part checkbox*/}
+
+				<div className="m-2 checkbox text-center">
+					<input type="checkbox" className="checkbox-round m-2"></input>
+					<span>By creating an account, you have read and agree to our <a>Terms</a> and <a>Privacy Policy</a></span>
+				</div>
+
+				{/* /////// fifth part button and modal*/}
+
+				<div className="text-center m-2">
+					<button onClick={toggleShow} className="buttonmainpage m-2 mb-5">Vertically centered modal</button>
+
+					<MDBModal tabIndex='-1' show={centredModal} setShow={setCentredModal}>
+						<MDBModalDialog centered>
+							<MDBModalContent>
+								<MDBModalBody className="text-center">
+								<img src={vector} className="logosingup"></img>
+								<h1>Woohoo</h1>
+									<p>Your registration is complete. Please, check your email for confirmation information.</p>
+									<p>Next, let’s set up your profile.</p>
+									<Link><button className="buttonmainpage">CREATE PROFILE</button></Link>
+								</MDBModalBody>
+							</MDBModalContent>
+						</MDBModalDialog>
+					</MDBModal>
+				</div>
+			</div>
 		</div>
 	);
 };
