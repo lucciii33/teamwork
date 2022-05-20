00ad92ae58295fc6bb060a09bcaad171a7dd8e34
lucciii33 <angelomaiele@gmail.com>
2022-05-20 14:29:29 +0000

    fixed problem

--- src/js/views/practitionerCategories.js
+++ src/js/views/practitionerCategories.js
@@ -9,6 +9,7 @@ import { MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
 
 
 
+
 export const PractitionerCategories = () => {
     const { store, actions } = useContext(Context);
 
@@ -22,115 +23,116 @@ export const PractitionerCategories = () => {
             </div>
 
 
-            <div className="purplecards">
+            <div className="purplecards d-flex align-items-center justify-content-center mb-5">
                 {/* fisrt card here */}
-                 <MDBContainer>
-                    <MDBRow className="">
-                        <MDBCol size='md' className=''>
-                        <div className="left">
-                            <div className="purpleboxes d-flex ">
-                                <img src={bodyimage}></img>
-                                <div className="d-block information">
-                                    <h6 className="titlebox">Bladder & Bowel Incontinence</h6>
-                                    <p className="paragraphbox">Accidental leakage of <br /> bodily fluids and waste</p>
+                <div class="d-flex align-items-center justify-content-center">
+                    <MDBCol>
+                    
+                                <div className="purpleboxes d-flex ">
+                                    <img src={bodyimage}></img>
+                                    <div className="d-block information">
+                                        <h6 className="titlebox">Bladder & Bowel Incontinence</h6>
+                                        <p className="paragraphbox">Accidental leakage of <br /> bodily fluids and waste</p>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                        </MDBCol>
-                        <MDBCol size='md' className=''>
-                            <div className="">
-                            <div className="purpleboxes d-flex">
-                                <img src={bodyimage}></img>
-                                <div className="d-block information">
-                                    <h6 className="titlebox">Bladder Pain</h6>
-                                    <p className="paragraphbox">Pain or pressure in the <br/>lower abdomen area and bladder</p>
+                           
+                    </MDBCol>
+                    <MDBCol>
+                                <div className="purpleboxes d-flex">
+                                    <img src={bodyimage}></img>
+                                    <div className="d-block information">
+                                        <h6 className="titlebox">Bladder Pain</h6>
+                                        <p className="paragraphbox">Pain or pressure in the <br />lower abdomen area and bladder</p>
+                                    </div>
                                 </div>
-                            </div>
-                            </div>
-                        </MDBCol>
-                        <MDBCol size='md' className=''>
-                        <div className="">
-                            <div className="purpleboxes d-flex">
-                                <img src={bodyimage}></img>
-                                <div className="d-block information">
-                                    <h6 className="titlebox">Constipation</h6>
-                                    <p className="paragraphbox">Straining pain during <br /> bowel movements </p>
+                        
+                    </MDBCol>
+                    <MDBCol>
+                    <div className="purpleboxes d-flex">
+                                    <img src={bodyimage}></img>
+                                    <div className="d-block information">
+                                        <h6 className="titlebox">Constipation</h6>
+                                        <p className="paragraphbox">Straining pain during <br /> bowel movements </p>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                        </MDBCol>
-                        </MDBRow>
-                </MDBContainer>
+                    </MDBCol>
+                </div>
 
-                    <MDBContainer>
-                      <MDBRow>  
-                        <MDBCol size='md' className=''>
-                            <div className="left">
-                            <div className="purpleboxes d-flex">
-                                <img src={bodyimage}></img>
-                                <div className="d-block information">
-                                    <h6 className="titlebox">Endometriosis</h6>
-                                    <p className="paragraphbox">Pain that occurs especially<br /> during menstrual periods</p>
+                {/* /////////////////// */}
+
+                <div class="d-flex align-items-center justify-content-center">
+                    <MDBCol>
+                    
+                    <div className="purpleboxes d-flex">
+                                    <img src={bodyimage}></img>
+                                    <div className="d-block information">
+                                        <h6 className="titlebox">Endometriosis</h6>
+                                        <p className="paragraphbox">Pain that occurs especially<br /> during menstrual periods</p>
+                                    </div>
                                 </div>
-                            </div>
-                            </div>
-                        </MDBCol>
-                        <MDBCol size='md' className='col-example'>
-                            <div className="purpleboxes d-flex">
+                           
+                    </MDBCol>
+                    <MDBCol>
+                    <div className="purpleboxes d-flex">
                                 <img src={bodyimage}></img>
                                 <div className="d-block information">
                                     <h6 className="titlebox">Menopause & Perimenopause</h6>
                                     <p className="paragraphbox">Irregular or pause of <br />  menstrual cycles </p>
                                 </div>
                             </div>
-                        </MDBCol>
+                        
+                    </MDBCol>
+                    <MDBCol>
                     
-                        <MDBCol size='md' className=''>
-                            <div className="purpleboxes d-flex">
+                    <div className="purpleboxes d-flex">
                                 <img src={bodyimage}></img>
                                 <div className="d-block information">
                                     <h6 className="titlebox">Painful Sex</h6>
-                                    <p className="paragraphbox">Recurrent genital pain that<br/> occurs just before, during, or after sex  </p>
+                                    <p className="paragraphbox">Recurrent genital pain that<br /> occurs just before, during, or after sex  </p>
                                 </div>
                             </div>
-                        </MDBCol>
-                        </MDBRow>
-                        </MDBContainer>
-                        
-                        <MDBContainer>
-                            <MDBRow>
-                        <MDBCol size='md' className=' '>
-                            <div className="">
-                            <div className="purpleboxes d-flex">
-                                <img src={bodyimage}></img>
-                                <div className="d-block information">
-                                    <h6 className="titlebox">Pelvic Organ Prolapse</h6>
-                                    <p className="paragraphbox">Drooping of pelvic organs <br/> such as  the bladder, uterus, vagina, etc. </p>
-                                </div>
-                            </div>
-                            </div>
-                        </MDBCol>
-                        <MDBCol size='md' className='col-example'>
-                            <div className="purpleboxes d-flex">
-                                <img src={bodyimage}></img>
-                                <div className="d-block information">
-                                    <h6 className="titlebox">Postpartum <br/> Recovery</h6>
-                                    <p className="paragraphbox">Recovery period after<br/> pregnancy/childbirth </p>
-                                </div>
-                            </div>
-                        </MDBCol>
-                        <MDBCol size='md' className=''>
-                            <div className="purpleboxes d-flex">
-                                <img src={bodyimage}></img>
-                                <div className="d-block information">
-                                    <h6 className="titlebox">Proactive Pelvic <br/> Care</h6>
-                                    <p className="paragraphbox">Methods to actively take <br/> care of your pelvic health </p>
-                                </div>
-                            </div>
-                        </MDBCol>
-                        </MDBRow>
-                        </MDBContainer>
+                    </MDBCol>
+                </div>
+
+
+
+
+                 {/* /////////////////// */}
+
+                 <div class="d-flex align-items-center justify-content-center">
+                    <MDBCol>
                     
+                    <div className="purpleboxes d-flex">
+                                    <img src={bodyimage}></img>
+                                    <div className="d-block information">
+                                        <h6 className="titlebox">Pelvic Organ Prolapse</h6>
+                                        <p className="paragraphbox">Drooping of pelvic organs <br /> such as  the bladder, uterus, vagina, etc. </p>
+                                    </div>
+                                </div>
+                           
+                    </MDBCol>
+                    <MDBCol>
+                    <div className="purpleboxes d-flex">
+                                <img src={bodyimage}></img>
+                                <div className="d-block information">
+                                    <h6 className="titlebox">Postpartum <br /> Recovery</h6>
+                                    <p className="paragraphbox">Recovery period after<br /> pregnancy/childbirth </p>
+                                </div>
+                            </div>
+                        
+                    </MDBCol>
+                    <MDBCol>
+                    
+                    <div className="purpleboxes d-flex">
+                                <img src={bodyimage}></img>
+                                <div className="d-block information">
+                                    <h6 className="titlebox">Proactive Pelvic <br /> Care</h6>
+                                    <p className="paragraphbox">Methods to actively take <br /> care of your pelvic health </p>
+                                </div>
+                            </div>
+                    </MDBCol>
+                </div>
+
             </div>
         </div>
 
